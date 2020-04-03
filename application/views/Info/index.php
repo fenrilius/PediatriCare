b8bf489584ac4a3408f31676b2e78704a689ddab
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-03 14:05:27 +0700

    Update index.php

--- application/views/Info/index.php
+++ application/views/Info/index.php
@@ -1,6 +1,15 @@
 
-<br><br>
+<!-- <br><br>
 <h1> THIS IS INFO</h1>
-<p>Kosong, cuman ada <?= $user['username']; ?></p>
-
+<p>Kosong, cuman ada <?= $user['username']; ?></p> -->
+<h2>Info</h2><br>
+<div class="media position-relative">
+    <img src="assets/info/covid19.jpg" class="mr-3">
+    <div class="media-body">
+        <h5 class="mt-3">Covid-19</h5>
+        <p>Covid-19 adalah penyakit yang ditimbulkan oleh infeksi virus corona baru atau SARS-CoV-2 yang berasal dari keluarga corona.
+            Namun, jenis virus yang menyebar kali ini belum pernah ada sebelumnya.</p>
+        <a href="#" class="stretched-link">Klik here to know...</a>
+    </div>
+</div>
 
