2dd8ce273bd30a5ee712681a965f247466728f04
bagasmillena <62025719+bagasmillena@users.noreply.github.com>
2020-04-03 11:08:54 +0700

    Add files via upload

---
+++ application/views/AboutUs.php
@@ -0,0 +1,6 @@
+
+<br><br>
+<h1> THIS IS ABOUT US</h1>
+<p>Tentang aku dan <?= $user['username']; ?></p>
+
+
---
+++ application/views/Info/index.php
@@ -0,0 +1,6 @@
+
+<br><br>
+<h1> THIS IS INFO</h1>
+<p>Kosong, cuman ada <?= $user['username']; ?></p>
+
+
