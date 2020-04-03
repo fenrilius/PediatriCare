79980df53114b6c9abb84b0e7b9f687f57237083
andrearahmadanisya <61450795+andrearahmadanisya@users.noreply.github.com>
2020-04-03 14:06:39 +0700

    Add files via upload

--- application/views/AboutUs.php
+++ application/views/AboutUs.php
@@ -1,6 +1,39 @@
-
-<br><br>
-<h1> THIS IS ABOUT US</h1>
-<p>Tentang aku dan <?= $user['username']; ?></p>
-
-
+<h2>About Us</h2><br><br>
+<div class="row row-cols-1 row-cols-md-2">
+    <div class="col mb-2">
+        <div class="card">
+            <img src="<?php echo base_url() . 'assets/images/dre.jpg' ?>" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Andrea Rahmadanisya</h5>
+                <p class="card-text">drea(1301184146) as a BackEnd.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-4">
+        <div class="card">
+            <img src="<?php echo base_url() . 'assets/images/bagas.jpg' ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Bagas Millen Alhafidz</h5>
+                <p class="card-text">bagas(1301180184) as a BackEnd</p>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-4">
+        <div class="card">
+            <img src="<?php echo base_url() . 'assets/images/rizka.jpg' ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Rizka Nur Octaviani</h5>
+                <p class="card-text">Rizka(1301184125) as a FrontEnd.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-4">
+        <div class="card">
+            <img src="<?php echo base_url() . 'assets/images/arya.jpg' ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Arya Pratama Anugerah</h5>
+                <p class="card-text">Arya(1301184336) as a FrontEnd.</p>
+            </div>
+        </div>
+    </div>
+</div>
